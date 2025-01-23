69456234b8e5b7be8d43a541218e0c4b72bf3a3d
Govind Khedkar <149473720+GovindTechie@users.noreply.github.com>
2025-01-23 21:11:25 +0530

    Update Hourglass.cpp

--- CPP_Language/Hourglass.cpp
+++ CPP_Language/Hourglass.cpp
@@ -1,19 +1,17 @@
+// Function to calculate the maximum hourglass sum in a 6x6 matrix
 int hourglassSum(vector<vector<int>> arr) {
-    vector <int> result;
+    vector<int> result;
 
-    
-    for(int i=0; i<4; i++) {
-        for(int j=0; j<4; j++) {
-            int add = arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            int add = arr[i][j] + arr[i][j+1] + arr[i][j+2]   // Top row
+                    + arr[i+1][j+1]                          // Middle element
+                    + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]; // Bottom row
+
             result.push_back(add);
         }
-        
-        
     }
-    
-    auto a = max_element(result.begin(), result.end());
-    
-    return *a;
-    
 
+    auto a = max_element(result.begin(), result.end());
+    return *a;
 }
