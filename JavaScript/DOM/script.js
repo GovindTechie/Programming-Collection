374d4b01bb7f43a6716eefe470544a12aec421e9
Govind Khedkar <149473720+GovindTechie@users.noreply.github.com>
2025-05-13 10:11:11 +0530

    Add files via upload

---
+++ JavaScript/DOM/script.js
@@ -0,0 +1,13 @@
+let heading = document.getElementById("heading");
+console.dir(heading);
+
+let headings = document.getElementsByClassName("headingClass")
+console.dir(headings)
+console.log(headings)
+
+
+let para = document.getElementsByTagName("p")
+console.log(para)
+
+let element = document.querySelector("p")
+console.log(element)
